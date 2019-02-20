00fe3b4387db27b5a164bb94cd0e00adee799211
Tchakatak <maxime.cheminade@systechno.fr>
2019-02-20 20:34:55 +0100

    Adding to mac changer options for ip or legacy ifconfig to change mac address

--- macchanger.py
+++ macchanger.py
@@ -9,15 +9,17 @@ import random
 #Parsing the user entry
 def get_arguments():
     parser = optparse.OptionParser()
-    parser.add_option("-i", "--interface", dest="interface", help="Interface to apply")
-    parser.add_option("-m", "--mac", dest="new_mac", help="New MAC address or RANDOM for a random mac")
+    parser.add_option("-i", "--interface", dest="interface", help="Interface")
+    parser.add_option("-m", "--mac", dest="new_mac", help="Set your new mac , leave it for a random generated mac")
     parser.add_option("-v", "--verbose", action="store_true", dest="verbose", default="false", help="Activate verbose mode")
+    parser.add_option("-l", "--legacy", action="store_true", default="false", dest="legacy", help="Legacy, use old ifconfig instead of ip")
+
     (options, arguments) = parser.parse_args()
     if not options.interface:
-        parser.error("[-] Please specify an interface, use --help for more info.")
+        parser.error("[-] Please specify an interface -i [INTERFACE], use --help for more info.")
         exit(0)
     elif not options.new_mac:
-        parser.error("[-] Please specify a mac address or autogen a random mac, use --help for more info.")
+        parser.error("[-] Please specify a mac address or autogen a random mac using -m option, use --help for more info.")
         exit(0)
     return options
 
@@ -32,14 +34,24 @@ def gen_00mac():
 
 #Change mac to given arguments
 def change_mac(interface, new_mac):
-    print("[+] Changing MAC address for " + interface + " to " + new_mac)
+    print("[+] Changing MAC address for " + interface + " to " + new_mac + " using ip")
+    subprocess.call(["ip", "link", "set", interface, "down" ])
+    subprocess.call(["ip", "link", "set", interface, "address", new_mac ])
+    subprocess.call(["ip", "link", "set", interface, "up" ])
+
+def change_mac_legacy(interface, new_mac):
+    print("[+] Changing MAC address for " + interface + " to " + new_mac + " using ifconfig")
     subprocess.call(["ifconfig", interface, "down"])
     subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
     subprocess.call(["ifconfig", interface, "up"])
 
 #Get current mac of the interface provided by user
-def get_current_mac(interface):
-    ifconfig_result = subprocess.check_output(["ifconfig", interface])
+def get_current_mac(interface,legacy):
+    if legacy == True :
+        ifconfig_result = subprocess.check_output(["ifconfig", interface])
+    else:
+        ifconfig_result = subprocess.check_output(["ip", "link", "show", interface])
+
     mac_address_search_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", ifconfig_result)
     if mac_address_search_result:
         return mac_address_search_result.group(0)
@@ -50,7 +62,7 @@ def get_current_mac(interface):
 options = get_arguments()
 
 #Get the current mac used by selected interface
-current_mac = get_current_mac(options.interface)
+current_mac = get_current_mac(options.interface, options.legacy)
 previous_mac = current_mac
 
 if options.verbose == True :
@@ -69,10 +81,13 @@ if options.new_mac == "RANDOM":
     print("[+] Random MAC Generated : " + str(options.new_mac))
 
 #Change mac
-change_mac(options.interface, options.new_mac)
+if options.legacy == True :
+    change_mac_legacy(options.interface, options.new_mac)
+else:
+    change_mac(options.interface, options.new_mac)
 
 #Get the current mac used by selected interface
-current_mac = get_current_mac(options.interface)
+current_mac = get_current_mac(options.interface, options.legacy)
 
 #Check if the mac address was changed and print a message for the user
 if current_mac != previous_mac:
